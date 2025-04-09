755d60dcc464cb7dc0c6ff3915dcce82e27a387a
Benjamin Gutman <benjamingutman@icloud.com>
2025-04-09 15:57:36 -0400

    Started question 2

---
+++ Question 2/DataStorage.h
@@ -0,0 +1,24 @@
+/*
+ * DataStorage.h
+ *
+ *  Created on: Apr 9, 2025
+ *      Author: benjamin
+ */
+
+#ifndef QUESTION_2_DATASTORAGE_H_
+#define QUESTION_2_DATASTORAGE_H_
+
+class DataStorage{
+private:
+	int* iPtr;
+	char* cPtr;
+	float* fPtr;
+
+public:
+	DataStorage(const char&, );
+
+};
+
+
+
+#endif /* QUESTION_2_DATASTORAGE_H_ */
